ee9ee114a3425fe4406dacc0a11f6f3acd64313d
dabing <passerbycrk@gmail.com>
2017-03-15 16:00:10 +0800

    deployment_target:10.10

--- Podfile.rb
+++ Podfile.rb
@@ -3,7 +3,7 @@ source 'https://github.com/CocoaPods/Specs.git'
 # 创建Podfile:
 # ln -fs Podfile.rb Podfile
 
-platform :osx, '10.12'
+platform :osx, '10.10'
 target 'MacWeChatTimeLinePlugin' do
     pod 'JRSwizzle', '~> 1.0'
 end
